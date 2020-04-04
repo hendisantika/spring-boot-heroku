0b94871ba2336a699e6968576f7ecd4d5c516df8
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-04 10:04:39 +0700

    #26 - Adjust Liquibase Migration Script

---
+++ src/main/resources/db/changelog/migration/V1_20200404_Init_Data.sql
@@ -0,0 +1,20 @@
+INSERT INTO record (id, data)
+VALUES (1, 'Uzumaki Naruto');
+INSERT INTO record (id, data)
+VALUES (2, 'Hatake Kakashi');
+INSERT INTO record (id, data)
+VALUES (3, 'Hendi Santika');
+INSERT INTO record (id, data)
+VALUES (4, 'Hikari Kireina Sakhy');
+INSERT INTO record (id, data)
+VALUES (5, 'Testing');
+INSERT INTO record (id, data)
+VALUES (6, 'Minato Namikaze');
+INSERT INTO record (id, data)
+VALUES (7, 'Sarutobi Sandaime');
+INSERT INTO record (id, data)
+VALUES (8, 'Uchiha Madara');
+INSERT INTO record (id, data)
+VALUES (9, 'Uchiha Sishui');
+INSERT INTO record (id, data)
+VALUES (10, 'Uchiha Itachi');
